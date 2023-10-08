e0ba37300365d02681d6678b1ecb156f1efcc306
Rodrigo Miranda <129284231+rodrigomirandaa@users.noreply.github.com>
2023-10-08 19:36:19 -0300

    Add files via upload

---
+++ atv7/ContaPoupanca.js
@@ -0,0 +1,7 @@
+import { ContaBancaria } from "./ContaBancaria.js";
+
+export class ContaPoupanca extends ContaBancaria(){
+    constructor(numero,agencia,saldo){
+        super(numero,agencia,"Conta Poupança",saldo)
+    }
+}
