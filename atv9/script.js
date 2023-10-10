bd0060fbc4c6d8292c8ad10447a955fb1a191cca
rodrigomirandaa <digoracm@gmail.com>
2023-10-10 08:57:37 -0300

    dropdown and visualize function

--- atv9/script.js
+++ atv9/script.js
@@ -1,3 +1,7 @@
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaUniversitaria } from "./ContaUniversitaria";
+
 //array p armazenas as contas
 const contas= [];
 
@@ -12,17 +16,48 @@ if(!numero || !agencia || !tipo || isNaN(saldo)){
     alert("Preencha todos os campos corretamente!");
     return;
 }
+
+
 //criando objeto novaConta
-const novaConta = {
-    numero,
-    agencia,
-    tipo,
-    saldo
-};
+let novaConta;
+
+switch(tipo){
+    case "Conta Corrente":
+        novaConta = new ContaCorrente(numero,agencia,saldo,"Platinum");
+        break;
+    case "Conta Poupanca":
+        novaConta = new ContaPoupanca(numero,agencia,saldo);
+        break;
+    case "Conta Universitária":
+        novaConta = new ContaUniversitaria(numero,agencia,saldo);
+        break;
+}
+
 //adicionando ao vetor contas
 contas.push(novaConta);
 
-console.log(novaConta);
-console.log(contas);
+//limpando os campos
+document.getElementById("numero").value = "";
+document.getElementById("agencia").value = "";
+document.getElementById("tipo").value = "Conta Corrente";
+document.getElementById("saldo").value = "";
+
+alert("Conta inserida com sucesso!");
+}
+
+function visualizarContas(){
+    const listaContas = document.getElementById("listacontas");
+    //limpando o logger das contas
+    listaContas.innerHTML= "";
+    //percorrendo o vetor contas[]
+    for(const conta of contas){
+        //criando um elemento <li>
+        const itemLista = document.createElement("li");
+        itemLista.textContent = `Número: ${conta.numero}, Agência: ${conta.agencia}, Tipo: ${conta.tipo}, Saldo: R$ ${conta.saldo.toFixed(2)}`;
+        listaContas.appendChild(itemLista);
+    }
 
 }
+
+console.log(novaConta);
+console.log(contas);
