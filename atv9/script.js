c9bb164bfde23d0d8efac31a160a9b85217b7594
rodrigomirandaa <digoracm@gmail.com>
2023-10-11 08:47:08 -0300

    export function

--- atv9/script.js
+++ atv9/script.js
@@ -3,56 +3,55 @@ import { ContaPoupanca } from "./ContaPoupanca.js";
 import { ContaUniversitaria } from "./ContaUniversitaria.js";
 
 //array p armazenas as contas
-const contas= [];
+const contas = [];
 
-function inserirConta(){
-    alert("sd");
-    
+export function inserirConta() {
+
     const numero = document.getElementById("numero").value;
     const agencia = document.getElementById("agencia").value;
     const tipo = document.getElementById("tipo").value;
     const saldo = parseFloat(document.getElementById("saldo").value);
 
-//verificando se todos os campos foram preenchidos
-if(!numero || !agencia || !tipo || isNaN(saldo)){
-    alert("Preencha todos os campos corretamente!");
-    return;
+    //verificando se todos os campos foram preenchidos
+    if (!numero || !agencia || !tipo || isNaN(saldo)) {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+
+
+    //criando objeto novaConta
+    let novaConta;
+
+    switch (tipo) {
+        case "Conta Corrente":
+            novaConta = new ContaCorrente(numero, agencia, saldo, false);
+            break;
+        case "Conta Poupanca":
+            novaConta = new ContaPoupanca(numero, agencia, saldo);
+            break;
+        case "Conta Universitária":
+            novaConta = new ContaUniversitaria(numero, agencia, saldo);
+            break;
+    }
+
+    //adicionando ao vetor contas
+    contas.push(novaConta);
+
+    //limpando os campos
+    document.getElementById("numero").value = "";
+    document.getElementById("agencia").value = "";
+    document.getElementById("tipo").value = "Conta Corrente";
+    document.getElementById("saldo").value = "";
+
+    alert("Conta inserida com sucesso!");
 }
 
-
-//criando objeto novaConta
-let novaConta;
-
-switch(tipo){
-    case "Conta Corrente":
-        novaConta = new ContaCorrente(numero,agencia,saldo,false);
-        break;
-    case "Conta Poupanca":
-        novaConta = new ContaPoupanca(numero,agencia,saldo);
-        break;
-    case "Conta Universitária":
-        novaConta = new ContaUniversitaria(numero,agencia,saldo);
-        break;
-}
-
-//adicionando ao vetor contas
-contas.push(novaConta);
-
-//limpando os campos
-document.getElementById("numero").value = "";
-document.getElementById("agencia").value = "";
-document.getElementById("tipo").value = "Conta Corrente";
-document.getElementById("saldo").value = "";
-
-alert("Conta inserida com sucesso!");
-}
-
-function visualizarContas(){
+export function visualizarContas() {
     const listaContas = document.getElementById("listacontas");
     //limpando o logger das contas
-    listaContas.innerHTML= "";
+    listaContas.innerHTML = "";
     //percorrendo o vetor contas[]
-    for(const conta of contas){
+    for (const conta of contas) {
         //criando um elemento <li>
         const itemLista = document.createElement("li");
         itemLista.textContent = `Número: ${conta.numero}, Agência: ${conta.agencia}, Tipo: ${conta.tipo}, Saldo: R$ ${conta.saldo.toFixed(2)}`;
